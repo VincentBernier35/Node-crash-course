3bcf207f23aee8785385cb047a4e0a5374ec3eca
VincentBernier35 <vincent.bernier@hotmail.fr>
2021-08-30 11:54:36 +0200

    I carried on this crash-course & i'm in the middle of the step 9/12.

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+
 
 
 
 // express app
 const app = express();
+
+
+// connect to mongoDB
+const dbURI =  'mongodb+srv://Vincent:Coucou223511@cluster0.jbqrn.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => console.log('connected to db'))
+    .catch((err) => console.log(err));
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -14,6 +25,9 @@ app.set('view engine', 'ejs');
 app.listen(3000);
 
 
+//Middleware & static file 
+
+app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
