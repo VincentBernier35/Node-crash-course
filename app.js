3dcb2b1c7a5acd70dd980cb458fc5214c14e11a1
VincentBernier35 <vincent.bernier@hotmail.fr>
2021-09-12 22:13:24 +0200

    on step 9/12

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
 
 
 // express app
 const app = express();
 
+
+
+
 //register view engine
 app.set('view engine', 'ejs');
 
-//listen for requests
-app.listen(3000);
+// midlewares & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
+//mongoose &mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+})
+
+
+
+
+
+//routes
+// home page / blog page
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consctetur'},
@@ -20,12 +50,13 @@ app.get('/', (req, res) => {
     res.render('index', { title: 'home', blogs});
 });
 
-
+// about page
 app.get('/about', (req, res) => {
     res.render('about', { title: 'about'});
 });
 
 
+// create page / new post
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'create a new blog'} );
 });
