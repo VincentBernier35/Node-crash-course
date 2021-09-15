c1c3cd084d1a6f073ff5ae7ae7c543c214ba00b6
VincentBernier35 <vincent.bernier@hotmail.fr>
2021-09-15 17:52:10 +0200

    the course is finished ! I hid the database password

--- app.js
+++ app.js
@@ -1,68 +1,45 @@
+// mongodb+srv://Vincent:Biscarosse2235@vincent.jbqrn.mongodb.net/node-tuts?retryWrites=true&w=majority
+
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
-
-
+const blogRoutes = require('./routes/blogRoutes');
 
 // express app
 const app = express();
 
+// connect to mongodb & listen for requests
+const dbURI = "mongodb+srv://Vincent:******@vincent.jbqrn.mongodb.net/node-tuts?retryWrites=true&w=majority";
 
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(result => app.listen(3000))
+  .catch(err => console.log(err));
 
-
-//register view engine
+// register view engine
 app.set('view engine', 'ejs');
 
-// midlewares & static files
+// middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
-//mongoose &mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        });
-})
-
-
-
-
-
-//routes
-// home page / blog page
+// routes
 app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'lorem ipsum dolor sit amet consctetur'},
-        {title: 'Mario finds stars', snippet: 'lorem ipsum dolor sit amet consctetur'},
-        {title: 'How to defeat bowser', snippet: 'lorem ipsum dolor sit amet consctetur'},
-    ];
-    res.render('index', { title: 'home', blogs});
+  res.redirect('/blogs');
 });
 
-// about page
 app.get('/about', (req, res) => {
-    res.render('about', { title: 'about'});
+  res.render('about', { title: 'About' });
 });
 
+// blog routes
+app.use('/blogs', blogRoutes);
 
-// create page / new post
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'create a new blog'} );
-});
-
-
-//404 pages
+// 404 page
 app.use((req, res) => {
-     res.status(404).render('404', { title: '404'});
+  res.status(404).render('404', { title: '404' });
 });
